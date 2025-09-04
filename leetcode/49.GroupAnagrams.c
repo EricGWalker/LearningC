108e3e29a60beb3c34624edefd4758d5a15f7ef6
Eric Walker <ericwalk@iu.edu>
2025-09-03 21:58:44 -0400

    finished getHashSet

--- leetcode/49.GroupAnagrams.c
+++ leetcode/49.GroupAnagrams.c
@@ -15,7 +15,7 @@ typedef struct node {
 typedef struct {
   // This is the key
   // This will have a fixed size of 26
-  unsigned int charCountArray[letterCount];
+  unsigned int *charCountArray;
   unsigned long long hash;
   node *stringsLinkedList;
 } hashSet;
@@ -37,9 +37,9 @@ unsigned long long hash(unsigned int *charCountArray) {
   unsigned long long hash = FNV_offset_basis;
   // I hope this works, and if it does. I'm sorry.
   // This is so hacky and bad
-  for (uint8_t *p = (uint8_t *)charCountArray;
-       p < ((uint8_t *)charCountArray + (letterCount * sizeof(unsigned int)));
-       p++) {
+  uint8_t *charCountArrayEnd =
+      ((uint8_t *)charCountArray + (letterCount * sizeof(unsigned int)));
+  for (uint8_t *p = (uint8_t *)charCountArray; p < charCountArrayEnd; p++) {
     hash *= FNV_prime;
     hash ^= *p;
   }
@@ -55,6 +55,9 @@ hashMap *createHashMap(unsigned int capacity) {
   return hashMap_p;
 }
 
+/*
+ * Doubles capacity of a passed in hashed map
+ */
 void doubleHashMapCapacity(hashMap *hashMap_p) {
   unsigned int newCapacity = hashMap_p->capacity * 2;
   hashSet *newHashSetArray = calloc(newCapacity, sizeof(hashSet));
@@ -81,6 +84,72 @@ void doubleHashMapCapacity(hashMap *hashMap_p) {
   hashMap_p->hashSetArray = newHashSetArray;
 }
 
+/*
+ * Inserts a hashSet into a hashMap and doubles capacity if needed
+ */
+void insertHashSet(hashSet hashSetObject, hashMap *hashMap_p) {
+  // Check if we need to double capacity
+  if (hashMap_p->size + 1 > hashMap_p->capacity / 2.0) {
+    doubleHashMapCapacity(hashMap_p);
+  }
+  // begin linear probing
+  for (unsigned int offset = 0; offset < hashMap_p->capacity; offset++) {
+    unsigned int targetIndex =
+        hashSetObject.hash + offset % hashMap_p->capacity;
+    hashSet *targetValue_p = &hashMap_p->hashSetArray[targetIndex];
+    if (targetValue_p == NULL) {
+      hashMap_p->hashSetArray[targetIndex] = hashSetObject;
+      break;
+    } else {
+      continue;
+    }
+  }
+  hashMap_p->size++;
+}
+/*
+ * Generates a new hashSet for the unique charCountArray
+ */
+hashSet generateHashSet(unsigned int *charCountArray) {
+  hashSet hashSetObject = {.charCountArray = charCountArray,
+                           .hash = hash(charCountArray),
+                           .stringsLinkedList = NULL};
+  return hashSetObject;
+}
+
+/*
+ * Determines if two arrays are equal
+ */
+bool arraysEqual(unsigned int *array1, unsigned int *array2,
+                 unsigned int sizeOfArray) {
+  for (unsigned int i = 0; i < sizeOfArray; i++) {
+    if (array1[i] != array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/*
+ * Checks if the charCountArray exists in the hashMap and returns the hashSet
+ */
+hashSet *getHashSet(unsigned int *charCountArray, hashMap *hashMap_p) {
+  unsigned long long charHash = hash(charCountArray);
+  // begin linear probing
+  for (unsigned int offset = 0; offset < hashMap_p->capacity; offset++) {
+    unsigned int targetIndex = charHash + offset % hashMap_p->capacity;
+    hashSet *targetValue_p = &hashMap_p->hashSetArray[targetIndex];
+    if (targetValue_p == NULL) {
+      return NULL;
+    } else if (arraysEqual(charCountArray, targetValue_p->charCountArray,
+                           letterCount)) {
+      return targetValue_p;
+    } else {
+      continue;
+    }
+  }
+  return NULL;
+}
+
 /**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
@@ -96,7 +165,7 @@ int main() {
   unsigned int letters[26] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
   unsigned int letters2[26] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2};
+                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
   printf("%llu\n", hash(letters));
   printf("%llu\n", hash(letters2));
 }
