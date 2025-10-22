74cdf09b9548dcd164319557944e440aedf60b12
Eric Walker <ezmoswalker@gmail.com>
2025-10-21 20:49:04 -0400

    It's been a couple of months since I've worked on this. I think I remember it being broken

--- DataStructures/AVL.c
+++ DataStructures/AVL.c
@@ -21,22 +21,6 @@ typedef struct __stackNode {
   struct __stackNode *next;
 } stackNode;
 
-long maxLong(long x, long y) { return (x > y) ? x : y; }
-
-long getHeight(avlNode *avlNode_p) {
-  // This should realistically never be executed, but just in-case here it goes.
-  if (avlNode_p == NULL) {
-    return -1;
-  }
-  // Doing this for code readability
-  avlNode *leftChild_p = avlNode_p->leftChild_p;
-  avlNode *rightChild_p = avlNode_p->rightChild_p;
-  long leftHeight = (leftChild_p) ? leftChild_p->height : -1;
-  long rightHeight = (rightChild_p) ? rightChild_p->height : -1;
-  long height = 1 + maxLong(leftHeight, rightHeight);
-  return height;
-}
-
 /*
  * Allocates Memory for a new leaf node and returns a pointer to it
  */
@@ -48,6 +32,50 @@ avlNode *createNewAVLLeafNode(long value) {
   newAVLNode->leftChild_p = NULL;
   newAVLNode->rightChild_p = NULL;
   return newAVLNode;
+}
+
+long maxLong(long x, long y) { return (x > y) ? x : y; }
+
+/*
+ * Updates the height of the current AVLNode given
+ * The children have the correct height or are null.
+ */
+void updateHeight(avlNode *avlNode_p) {
+  // This should never be called but putting here just in case.
+  if (avlNode_p == NULL) {
+    return;
+  }
+  // Doing this for code readability
+  avlNode *leftChild_p = avlNode_p->leftChild_p;
+  avlNode *rightChild_p = avlNode_p->rightChild_p;
+  long leftHeight = (leftChild_p) ? leftChild_p->height : -1;
+  long rightHeight = (rightChild_p) ? rightChild_p->height : -1;
+  long height = 1 + maxLong(leftHeight, rightHeight);
+  avlNode_p->height = height;
+  return;
+}
+
+/* NOTE: I realize that I could write a function updateHeightAndGetBalance and
+ * save some executions But I think that would make the code I'm writing less
+ * maintainable / readable I'm not sure for now because I don't know how to
+ * write beautiful code, but I'm trying to figure it out
+ */
+
+/*
+ * Returns the correct balance of an avlNode given
+ * both the child nodes have the correct height stored
+ */
+int getBalance(avlNode *avlNode_p) {
+  // Re Implementing some of the same logic from updateHeight b/c we assume that
+  // the child node's height is correct.
+  avlNode *leftChild_p = avlNode_p->leftChild_p;
+  avlNode *rightChild_p = avlNode_p->rightChild_p;
+  long leftHeight = (leftChild_p) ? leftChild_p->height : -1;
+  long rightHeight = (rightChild_p) ? rightChild_p->height : -1;
+  // WOW WHAT A MICRO-OPTIMIZATION CHANGING BALANCE TO AN INT ROFL
+  // As if my code is performant, even though I try to make it performant *sigh*
+  int balance = leftHeight - rightHeight;
+  return balance;
 }
 
 /*
@@ -63,6 +91,52 @@ void initializeEmptyAVLTree(long rootValue, avlTree *avlTree_p) {
   avlTree_p->valueCount = 1;
 }
 
+/* All credit for my ability to implement this goes to  whoever qingyi is:
+ * https://pages.cs.wisc.edu/~qingyi/
+ * The graphic image on this page makes tracking subtrees easy to visualize
+ */
+
+/*
+ * Rotates a subtree that is unbalanced by skew of left left.
+ * grandParentChild_pp is the address of the pointer to the child of the
+ * grandparent or the AVLTree root pointer if changing tree root
+ */
+void rotateLeftLeft(avlNode *oldSubTreeRoot_p, avlNode **grandParentChild_pp) {
+  avlNode *newSubTreeRoot_p = oldSubTreeRoot_p->leftChild_p;
+  *grandParentChild_pp = newSubTreeRoot_p;
+  oldSubTreeRoot_p->leftChild_p = newSubTreeRoot_p->rightChild_p;
+  newSubTreeRoot_p->rightChild_p = oldSubTreeRoot_p;
+  // I think this is the only height I need to update
+  // will probably have to fix later when I'm wrong.
+  updateHeight(oldSubTreeRoot_p);
+}
+/*
+ * Rotates a subtree that is unbalanced by skew of right right.
+ * grandParentChild_pp is the address of the pointer to the child of the
+ * grandparent or the AVLTree root pointer if changing tree root
+ */
+void rotateRightRight(avlNode *oldSubTreeRoot_p,
+                      avlNode **grandParentChild_pp) {
+  avlNode *newSubTreeRoot_p = oldSubTreeRoot_p->rightChild_p;
+  *grandParentChild_pp = newSubTreeRoot_p;
+  oldSubTreeRoot_p->rightChild_p = newSubTreeRoot_p->leftChild_p;
+  newSubTreeRoot_p->leftChild_p = oldSubTreeRoot_p;
+  // I think this is the only height I need to update
+  // will probably have to fix later when I'm wrong.
+  updateHeight(oldSubTreeRoot_p);
+}
+void rotateRightLeft(avlNode *oldSubTreeRoot_p, avlNode **grandParentChild_pp) {
+  avlNode *newSubTreeRoot_p = oldSubTreeRoot_p->leftChild_p;
+  *grandParentChild_pp = newSubTreeRoot_p;
+  oldSubTreeRoot_p->leftChild_p = newSubTreeRoot_p->rightChild_p;
+  newSubTreeRoot_p->rightChild_p = oldSubTreeRoot_p;
+  updateHeight(oldSubTreeRoot_p);
+  updateHeight(newSubTreeRoot_p);
+  // TODO: Wipe out all the rotation functions and redo them.
+}
+void rotateLeftRight(avlNode *subTreeRoot_p) {
+  // TODO:
+}
 /*
  * Insert a value into the AVL Tree
  */
@@ -123,8 +197,18 @@ void avlInsert(long value, avlTree *avlTree_p) {
     return;
   }
 
-  //TODO: Update height of all nodes
+  // This logic is going to be complicated so that we only traverse the
+  // traversalStack once instead of twice.
 
-  //TODO: Check Balance of ancestors and then perform necessary rotations.
+  while (visitedHead_p != NULL) {
+    avlNode *currentAVLNode = visitedHead_p->avlNode_p;
+    updateHeight(currentAVLNode_p);
+    int currentBalance = getBalance(currentAVLNode_p);
+    if (currentBalance > 1 || currentBalance < -1) {
+      // TODO: begin the rotations.
+    }
+  }
+
+  // TODO: Check Balance of ancestors and then perform necessary rotations.
 }
 // vim: ts=2 sts=2 sw=2 et
